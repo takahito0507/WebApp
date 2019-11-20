fa6de1410e5b7d43d3053116ca264019bd0b769c
unknown <ta.itou@x-all.nttr.co.jp>
2019-11-20 16:31:52 +0900

    added web app

---
+++ app/script/js/sacredLots.js
@@ -0,0 +1,19 @@
+  'use strict';
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener("click", () => {
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉'; // 5%
+    } else if (n < 0.25) {
+      btn.textContent = '中吉'; // 20%
+    } else if (n < 0.50) {
+      btn.textContent = '吉'; // 25%
+    } else if (n < 0.75) {
+      btn.textContent = '小吉'; // 25%
+    } else if (n < 0.95) {
+      btn.textContent = '凶'; // 20%
+    } else {
+      btn.textContent = '大凶'; // 5%
+    }
+  });
